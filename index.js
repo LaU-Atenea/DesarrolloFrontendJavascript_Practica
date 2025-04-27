55eb0e2feaa09a40de943c8109d4d8c796e2290b
Laura <lapca20@gmail.com>
2025-04-27 14:13:41 +0200

    Creo index.js y pruebo a ver los anuncios creados en el

---
+++ index.js
@@ -0,0 +1,23 @@
+function drawAnuncios(){
+const anuncios = 
+    [{"handler":"@usuario1","date":"2025-04-01T20:03:01.320Z","nombre":"Cama", "descripcion":"Eligendi fugiat veniam a neque omnis doloribus sequi.","precio":40,"tipo":"Compra"},
+    {"handler":"@usuario2","date":"2025-03-30T20: 03: 01.320Z", "nombre":"Ordenador", "descripcion":"Laborum ad animi officia dolore nisi necessitatibus a porro!","precio":3,"tipo":"Compra"},
+    {"handler":"@usuario3","date":"2025-03-29T20: 03: 01.320Z","nombre":"Telefono", "descripcion":"Ayer estuve en le parque y me lo pasé genial","precio":34,"tipo":"Venta"},
+    {"handler":"@usuario4","date":"2025-03-28T0: 03: 01.320Z","nombre":"Camiseta", "descripcion":"Lorem ipsum dolor sit amet consectetur adipisicing elit.","precio":400,"tipo":"Venta"}]
+
+anuncios.forEach((anuncio) => {
+    const container = document.querySelector(".anuncio-container")
+    const anuncioHtml = document.createElement("div");
+    anuncioHtml.innerHTML = `
+    <p>${anuncio.nombre}</p>
+    <p>${anuncio.descripcion}</p>
+    <p>${anuncio.precio}</p>
+    <p>${anuncio.tipo}</p>
+    `
+
+    container.appendChild(anuncioHtml)
+})
+
+}
+
+drawAnuncios();
